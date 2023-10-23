0e210151fb0f5a6c9fe5dce6dac326f704e24dd2
Nadzeya <nadzeya.zhukovich@gmail.com>
2023-10-23 14:42:02 +0200

    Deleted console.log

--- src/presenter/movie-list-presenter.js
+++ src/presenter/movie-list-presenter.js
@@ -47,8 +47,6 @@ export default class MovieListPresenter {
   }
 
   init() {
-    console.log('sourcedMovies =>', this.#sourcedMovies);
-
     this.#renderUser();
     this.#renderNavigation();
     this.#renderMovieBoard(this.#movies);
